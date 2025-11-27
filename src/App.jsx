65a950f959a45062103b1ec17568a030bf0c9e43
DanielArevalo22 <josesant768@gmail.com>
2025-11-27 09:49:11 -0500

    Render lazy login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from 'react'
 import MainPage from './pages/MainPage'
 import PokemonInfo from './pages/PokemonInfo'
 const PokeInventory = lazy (() => import('./pages/PokemonList') )
+const Login = lazy( () => import('./pages/Login') );
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' element={<MainPage/>}></Route>
           <Route path='/pokemon/inventory' element={<PokeInventory/>}></Route>
           <Route path='/pokemon/info/:name' element={<PokemonInfo/>}></Route>
+          <Route path="/login" element= {<Login/>}></Route>
       </Routes>
     </BrowserRouter>
 
